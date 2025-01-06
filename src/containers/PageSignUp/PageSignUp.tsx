af738f1353e6148f3e44995974fd0a1b5e02de0e
Amna Ateef <84019787+amnaateef@users.noreply.github.com>
2025-01-06 19:00:13 +0500

    login and signup changes

--- src/containers/PageSignUp/PageSignUp.tsx
+++ src/containers/PageSignUp/PageSignUp.tsx
@@ -1,70 +1,46 @@
-import React, { FC } from "react";
-import facebookSvg from "images/Facebook.svg";
-import twitterSvg from "images/Twitter.svg";
-import googleSvg from "images/Google.svg";
+import React, { FC, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Input from "shared/Input/Input";
-import ButtonPrimary from "shared/Button/ButtonPrimary";
 import { Link } from "react-router-dom";
+
+interface ButtonPrimaryProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+const ButtonPrimary: React.FC<ButtonPrimaryProps> = ({
+  children,
+  onClick,
+  ...props
+}) => {
+  return (
+      <button onClick={onClick} {...props}>
+          {children}
+      </button>
+  );
+};
 
 export interface PageSignUpProps {
   className?: string;
 }
 
-const loginSocials = [
-  {
-    name: "Continue with Facebook",
-    href: "#",
-    icon: facebookSvg,
-  },
-  {
-    name: "Continue with Twitter",
-    href: "#",
-    icon: twitterSvg,
-  },
-  {
-    name: "Continue with Google",
-    href: "#",
-    icon: googleSvg,
-  },
-];
-
 const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
+
+  const [showAdditionalFields, setShowAdditionalFields] = useState(false);
+    const handleContinueClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      setShowAdditionalFields(true);
+  };
+
   return (
     <div className={`nc-PageSignUp  ${className}`} data-nc-id="PageSignUp">
       <Helmet>
-        <title>Sign up</title>
+        <title>Sign up | TaskEase</title>
       </Helmet>
       <div className="container mb-24 lg:mb-32">
         <h2 className="my-20 flex items-center text-3xl leading-[115%] md:text-5xl md:leading-[115%] font-semibold text-neutral-900 dark:text-neutral-100 justify-center">
           Signup
         </h2>
         <div className="max-w-md mx-auto space-y-6 ">
-          <div className="grid gap-3">
-            {loginSocials.map((item, index) => (
-              <a
-                key={index}
-                href={item.href}
-                className="nc-will-change-transform flex w-full rounded-lg bg-primary-50 dark:bg-neutral-800 px-4 py-3 transform transition-transform sm:px-6 hover:translate-y-[-2px]"
-              >
-                <img
-                  className="flex-shrink-0"
-                  src={item.icon}
-                  alt={item.name}
-                />
-                <h3 className="flex-grow text-center text-sm font-medium text-neutral-700 dark:text-neutral-300 sm:text-sm">
-                  {item.name}
-                </h3>
-              </a>
-            ))}
-          </div>
-          {/* OR */}
-          <div className="relative text-center">
-            <span className="relative z-10 inline-block px-4 font-medium text-sm bg-white dark:text-neutral-400 dark:bg-neutral-900">
-              OR
-            </span>
-            <div className="absolute left-0 w-full top-1/2 transform -translate-y-1/2 border border-neutral-100 dark:border-neutral-800"></div>
-          </div>
           {/* FORM */}
           <form className="grid grid-cols-1 gap-6" action="#" method="post">
             <label className="block">
@@ -83,8 +59,70 @@ const PageSignUp: FC<PageSignUpProps> = ({ className = "" }) => {
               </span>
               <Input type="password" className="mt-1" />
             </label>
-            <ButtonPrimary type="submit">Continue</ButtonPrimary>
+            <fieldset className="block">
+              <span className="text-neutral-800 dark:text-neutral-200">
+                Select Role
+              </span>
+              <div className="mt-1">
+                <label className="inline-flex items-center">
+                  <input type="radio" name="role" value="user" className="form-radio" />
+                  <span className="ml-2">User</span>
+                </label>
+                <label className="inline-flex items-center ml-6">
+                  <input type="radio" name="role" value="expert" className="form-radio" />
+                  <span className="ml-2">Expert</span>
+                </label>
+              </div>
+            </fieldset>
+            <ButtonPrimary type="button"
+            className="bg-indigo-600 text-white py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+             onClick={handleContinueClick}>
+             Continue
+          </ButtonPrimary>
           </form>
+
+          {showAdditionalFields && (
+            <form className="grid grid-cols-1 gap-6 mt-6" action="#" method="post">
+              <label className="block">
+                <span className="text-neutral-800 dark:text-neutral-200">
+                  First Name
+                </span>
+                <Input type="text" placeholder="First Name" className="mt-1" />
+              </label>
+              <label className="block">
+                <span className="text-neutral-800 dark:text-neutral-200">
+                  Last Name
+                </span>
+                <Input type="text" placeholder="Last Name" className="mt-1" />
+              </label>
+              <label className="block">
+                <span className="text-neutral-800 dark:text-neutral-200">
+                  CNIC
+                </span>
+                <Input type="text" placeholder="CNIC" className="mt-1" />
+              </label>
+              <label className="block">
+                <span className="text-neutral-800 dark:text-neutral-200">
+                  Address
+                </span>
+                <Input type="text" placeholder="Address" className="mt-1" />
+              </label>
+              <label className="block">
+                <span className="text-neutral-800 dark:text-neutral-200">
+                  Gender
+                </span>
+                <select className="mt-1 block w-full">
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="other">Other</option>
+                </select>
+              </label>
+              <ButtonPrimary 
+              className="bg-indigo-600 text-white py-2 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+              type="submit">Submit</ButtonPrimary>
+            </form>
+          )}
+
 
           {/* ==== */}
           <span className="block text-center text-neutral-700 dark:text-neutral-300">
